c3a2a446143635188bc2081379df06062adc3c8a
gogyounglim <rhrudfla12445@naver.com>
2022-05-18 18:04:29 +0900

    20220518

--- js/main.js
+++ js/main.js
@@ -1,14 +1,47 @@
-// $(document).ready(function() {
-//     $(window).scroll( function(){
-//         $('container1').each( function(i){
+$(document).ready(function() {
+    $(window).scroll(function(){
+        $('.container1_1topText').each( function(i){
             
-//             var bottom_of_element = $(this).offset().top + $(this).outerHeight();
-//             var bottom_of_window = $(window).scrollTop() + $(window).height();
+            var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
             
-//             if( bottom_of_window > bottom_of_element ){
-//                 $(this).animate({'opacity':'1','margin-left':'0px'},1000);
-//             }
+            if( bottom_of_window > bottom_of_element ){
+                $('#container1').animate({'opacity':'1','margin':'0 auto'},1000);
+            }
             
-//         }); 
-//     });
-// });
+        }); 
+    });
+});
+
+$(document).ready(function() {
+    $(window).scroll(function(){
+        $('.container2_topText').each( function(i){
+            
+            var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            if( bottom_of_window > bottom_of_element ){
+                $('.container2_topText').animate({'opacity':'1','margin':'0 auto'},1000);
+            }
+            
+        }); 
+    });
+});
+
+$(document).ready(function() {
+    $(window).scroll(function(){
+        $('.container2').each( function(i){
+            
+            var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            if( bottom_of_window > bottom_of_element ){
+                $('.container2').animate({'opacity':'1','margin':'0 auto'},1000);
+            }
+            
+        }); 
+    });
+});
+
+
+
