5130b7f9a89716885d7a4c8003dda4c913a1e50e
gogyounglim <76462700+gogyounglim@users.noreply.github.com>
2022-05-23 09:27:10 +0900

    20220521

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {
+    $('.sliderText').animate({'opacity':'1'},1000);
+});
+
+
+
+$(document).ready(function() {
+
     $(window).scroll(function(){
-        $('.container1_1topText').each( function(i){
+        $('.container1_1topText').each( function(){
             
             var bottom_of_element = $(this).offset().top + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
@@ -11,36 +18,37 @@ $(document).ready(function() {
             
         }); 
     });
+
+    $(window).scroll(function(){
+        $('.container3Animate').each( function(){
+            
+            var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            if( bottom_of_window > bottom_of_element ){
+                $('#container3').animate({'opacity':'1','margin':'0 auto'},1000);
+            }
+            
+        }); 
+    });
 });
 
 $(document).ready(function() {
     $(window).scroll(function(){
-        $('.container2_topText').each( function(i){
+        $('.container2_topText').each( function(){
             
             var bottom_of_element = $(this).offset().top + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             
             if( bottom_of_window > bottom_of_element ){
                 $('.container2_topText').animate({'opacity':'1','margin':'0 auto'},1000);
+                $('#container2').animate({'opacity':'1','margin':'0 auto'},1000);
             }
             
         }); 
     });
-});
 
-$(document).ready(function() {
-    $(window).scroll(function(){
-        $('.container2').each( function(i){
-            
-            var bottom_of_element = $(this).offset().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
-            
-            if( bottom_of_window > bottom_of_element ){
-                $('.container2').animate({'opacity':'1','margin':'0 auto'},1000);
-            }
-            
-        }); 
-    });
+    
 });
 
 
