c1416c77d1043ffa316ad4ae275b8e7f25243f44
gogyounglim <76462700+gogyounglim@users.noreply.github.com>
2022-05-23 22:07:24 +0900

    20220523

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// 메인기본효과 
+
 $(document).ready(function() {
     $('.sliderText').animate({'opacity':'1'},1000);
 });
@@ -31,11 +33,26 @@ $(document).ready(function() {
             
         }); 
     });
+
+    $(window).scroll(function(){
+        $('.container4Animate').each( function(){
+            
+            var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            if( bottom_of_window > bottom_of_element ){
+                $('#container4').animate({'opacity':'1','margin':'0 auto'},1000);
+            }
+            
+        }); 
+    });
+
+
 });
 
 $(document).ready(function() {
     $(window).scroll(function(){
-        $('.container2_topText').each( function(){
+        $('.container2Animate').each( function(){
             
             var bottom_of_element = $(this).offset().top + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
@@ -47,9 +64,61 @@ $(document).ready(function() {
             
         }); 
     });
-
-    
+ 
 });
+
+
+
+// 메인페이지 슬라이드
+
+
+$(function() {
+    $('.next').click(function(){
+        if($('.slides').css('left')=='-390px' ){
+            $('.slides').stop().animate({left:-775},800, function(){
+
+                $('.slides li:first').appendTo('.slide');
+    
+            });
+        }     
+        else{
+            $('.slides').stop().animate({left:-390},400, function(){
+
+                $('.slides li:first').appendTo('.slide');
+    
+            });
+
+        }
+
+
+
+    });
+});
+
+$(function() {
+    $('.prev').click(function(){
+        if($('.slides').css('left')=='-775px' ){
+            $('.slides').stop().animate({left:-390},800, function(){
+
+                $('.slides li:first').appendTo('.slide');
+    
+            });
+        }     
+        else{
+            $('.slides').stop().animate({left:0},800, function(){
+
+                $('.slides li:first').appendTo('.slide');
+    
+            });
+
+        }
+
+
+
+    });
+});
+
+
 
 
 
