85724916585d6f828cdc5163fb9601892ff4f7e5
gogyounglim <76462700+gogyounglim@users.noreply.github.com>
2022-05-26 01:22:29 +0900

    052501

--- js/main.js
+++ js/main.js
@@ -179,6 +179,15 @@ $(function() {
     });
 });
 
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+      document.getElementById("navbar").style.top = "0";
+    } else {
+      document.getElementById("navbar").style.top = "-80px";
+    }
+  }
 
 
 
