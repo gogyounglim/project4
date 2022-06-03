37cec30c3a8ccf18d669650889c098811aa46add
gogyounglim <rhrudfla12445@naver.com>
2022-06-03 17:21:16 +0900

    20220603-02

--- js/main.js
+++ js/main.js
@@ -278,5 +278,34 @@ function scrollFunction() {
 });
 
 
+var menuLeft = document.getElementById( 'cbp-spmenu-s1' ),
+		menuRight = document.getElementById( 'cbp-spmenu-s2' ),
+        menuRight2 = document.getElementsByClassName('cbp-spmenu-right'),
+		menuTop = document.getElementById( 'cbp-spmenu-s3' ),
+		menuBottom = document.getElementById( 'cbp-spmenu-s4' ),
+		showLeft = document.getElementById( 'showLeft' ),
+		showRight = document.getElementById( 'showRight' ),
+		showTop = document.getElementById( 'showTop' ),
+		showBottom = document.getElementById( 'showBottom' ),
+		showLeftPush = document.getElementById( 'showLeftPush' ),
+		showRightPush = document.getElementById( 'showRightPush' ),
+		body = document.body;
+
+
+showRight.onclick = function() {
+    classie.toggle( this, 'active' );
+    classie.toggle( menuRight, 'cbp-spmenu-open' );
+    disableOther( 'showRight' );
+};
+
+function disableOther( button ) {
+
+	if( button !== 'showRight' ) {
+		classie.toggle( showRight, 'disabled' );
+	};
+
+}
+
+
 
 
