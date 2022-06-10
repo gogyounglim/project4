596899cdac1080ab237c503d315e46bd28b7c613
gogyounglim <rhrudfla12445@naver.com>
2022-06-10 17:56:35 +0900

    20220610

--- js/sub1.js
+++ js/sub1.js
@@ -1,37 +1,36 @@
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+//       document.getElementById("navbar").style.top = "0";
+//     } else {
+//       document.getElementById("navbar").style.top = "-80px";
+//     }
+//   }
+
 $(function() {
     $('.next').click(function(){
         if($('.slides').css('left')=='0px' ){
-            $('.slides').stop().animate({left:-300},200, function(){
+            $('.slides').stop().animate({left:-240},200, function(){
 
                 $('.slides li:first').appendTo('.slide');
     
             });
-        }     
-        
+        }        
         else{
 
         }
-
-
 
     });
-});
-
-$(function() {
     $('.prev').click(function(){
-        if($('.slides').css('left')=='-300px' ){
-            $('.slides').stop().animate({left:0},200, function(){
-
-                $('.slides li:first').appendTo('.slide');
-    
+        if($('.slides').css('left')=='-240px' ){
+            $('.slides').stop().animate({left:-0},200, function(){
+                $('.slides li:first').appendTo('.slide')
             });
-        }     
-        
+        }        
         else{
 
         }
-
-
 
     });
 });
